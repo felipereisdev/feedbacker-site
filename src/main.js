5e78eda46dad8ba0d5d3676ec011d6fe11b5afda
Felipe Reis <felipereis@Felipes-MacBook-Pro.local>
2021-08-16 19:16:41 +0100

    First commit

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/fonts.css'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
---
+++ src/views/Home/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <custom-header
+    @create-account="handleCreateAccount"
+    @login="handleLogin"
+  />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker @ 2021</p>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+import CustomHeader from '@/components/Home/CustomHeader.vue'
+import Contact from '@/components/Home/Contact.vue'
+
+export default {
+  components: {
+    CustomHeader,
+    Contact
+  },
+
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+
+    }
+
+    function handleCreateAccount () {
+
+    }
+
+    return {
+      handleLogin,
+      handleCreateAccount
+    }
+  }
+}
+</script>
